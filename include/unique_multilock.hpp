53c354a5d6cef4c76881b8b6ace09d70f45a40f5
Ted Lyngmo <ted@lyncon.se>
2025-08-06 07:58:25 +0200

    Use addressof and get<sizeof...(Ms) - 1>(m_ms)
    MSVC fails to compile an empty set of mutexes using:
    
      if constexpr (sizeof...(Ms) == 1) {
        res = std::get<0>(m_ms)->lock() ? -1 : 0;
      }
    
    but `std::get<sizeof...(Ms) - 1>(m_ms)` works.
    
    Signed-off-by: Ted Lyngmo <ted@lyncon.se>

--- include/unique_multilock.hpp
+++ include/unique_multilock.hpp
@@ -1,9 +1,9 @@
 #ifndef UNIQUE_MULTILOCK_HPP_A2BDEBD0_71FC_11F0_9AA4_90B11C0C0FF8
 #define UNIQUE_MULTILOCK_HPP_A2BDEBD0_71FC_11F0_9AA4_90B11C0C0FF8
-
 #include <chrono>
 #include <concepts>
 #include <cstdint>
+#include <memory>
 #include <mutex>
 #include <utility>
 
@@ -21,7 +21,8 @@ concept Lockable = BasicLockable<T> && requires(T t) {
 
 template<class T>
 concept TimedLockable = Lockable<T> && requires(T t) {
-    { // using an unusal ratio to avoid false positives for hardcoded duration types
+    { // using an unusal ratio to avoid false positives for hardcoded duration
+      // types
         t.try_lock_for(std::chrono::duration<std::int_least32_t, std::ratio<1, 999999999>>{})
     } -> std::same_as<bool>;
     { t.try_lock_until(std::chrono::steady_clock{}) } -> std::same_as<bool>;
@@ -45,22 +46,23 @@ public:
     }
     // rule of 5 end ----------------------------------------------------------
 
-    unique_multilock(Ms&... ms) : m_ms(&ms...) { lock(); }
-    unique_multilock(std::defer_lock_t, Ms&... ms) noexcept : m_ms(&ms...) {}
-    unique_multilock(std::adopt_lock_t, Ms&... ms) noexcept : m_ms(&ms...), m_locked(true) {}
+    unique_multilock(Ms&... ms) : m_ms(std::addressof(ms)...) { lock(); }
+    unique_multilock(std::defer_lock_t, Ms&... ms) noexcept : m_ms(std::addressof(ms)...) {}
+    unique_multilock(std::adopt_lock_t, Ms&... ms) noexcept : m_ms(std::addressof(ms)...), m_locked(true) {}
 
     unique_multilock(std::try_to_lock_t, Ms&... ms)
         requires(... && Lockable<Ms>)
-        : m_ms(&ms...), m_locked(try_lock() == -1) {}
+        : m_ms(std::addressof(ms)...), m_locked(try_lock() == -1) {}
 
     template<class Rep, class Period>
         requires(... && Lockable<Ms>) // TimedLockable?
-    unique_multilock(const std::chrono::duration<Rep, Period>& dur, Ms&... ms) : m_ms(&ms...), m_locked(try_lock_for(dur) == -1) {}
+    unique_multilock(const std::chrono::duration<Rep, Period>& dur, Ms&... ms) :
+        m_ms(std::addressof(ms)...), m_locked(try_lock_for(dur) == -1) {}
 
     template<class Clock, class Duration>
         requires(... && Lockable<Ms>) // TimedLockable?
     unique_multilock(const std::chrono::time_point<Clock, Duration>& tp, Ms&... ms) :
-        m_ms(&ms...), m_locked(try_lock_until(tp) == -1) {}
+        m_ms(std::addressof(ms)...), m_locked(try_lock_until(tp) == -1) {}
 
     void swap(unique_multilock& other) noexcept {
         std::swap(m_ms, other.m_ms);
@@ -76,7 +78,7 @@ public:
 
     void lock() {
         if constexpr(sizeof...(Ms) == 1) {
-            std::get<0>(m_ms)->lock();
+            std::get<sizeof...(Ms) - 1>(m_ms)->lock();
         } else if constexpr(sizeof...(Ms) > 1) {
             std::apply([](auto... ms) { std::lock(*ms...); }, m_ms);
         }
@@ -91,7 +93,7 @@ public:
     {
         int res = -1;
         if constexpr(sizeof...(Ms) == 1) {
-            res = std::get<0>(m_ms)->try_lock() ? -1 : 0;
+            res = std::get<sizeof...(Ms) - 1>(m_ms)->try_lock() ? -1 : 0;
         } else if constexpr(sizeof...(Ms) > 1) {
             res = std::apply([](auto... ms) { return std::try_lock(*ms...); }, m_ms);
         }
