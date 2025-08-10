cec0f069a74b04d526e5f741db5ca6cd1a7bdf07
Ted Lyngmo <ted@lyncon.se>
2025-08-10 10:58:00 +0200

    Remove recursion in try_lock_until
    Replaced the recursive calls with a compiletime tuple of rotating index
    sequences.
    
    This should allow for pretty efficient locking of well over 100 mutexes
    and compile times aren't that bad either.
    
    MSVC runs out of heap space with
    make_tuple_of_rotating_index_sequences<129>.
    
    Signed-off-by: Ted Lyngmo <ted@lyncon.se>

--- include/try_lock_for_until.hpp
+++ include/try_lock_for_until.hpp
@@ -3,65 +3,73 @@
 #include "detail/concepts.hpp"
 
 #include <chrono>
-#include <concepts>
 #include <cstdint>
 #include <tuple>
 #include <utility>
 namespace lyn {
 namespace detail {
+    namespace rot_detail {
+        template<class...>
+        struct rot_help;
 
-    enum class rec_res { no, yes, timeout };
+        template<>
+        struct rot_help<std::index_sequence<>> {
+            using type = std::tuple<>;
+        };
 
-    template<class Tup, std::size_t... Is>
-    void unlock(Tup& locks, std::index_sequence<Is...>, std::size_t lock_co) {
-        // lock_co must be at least 1
-        (... && ((std::get<Is>(locks).unlock(), true) && --lock_co));
-    }
+        template<size_t I0, size_t... Is>
+        struct rot_help<std::index_sequence<I0, Is...>> :
+            rot_help<std::index_sequence<Is..., I0>, std::index_sequence<I0, Is...>, std::index_sequence<I0, Is...>> {};
 
-    template<class Clock, class Dur, class Tup, std::size_t I0, std::size_t... Is, class End>
-    rec_res try_lock_until_rec_rot(const std::chrono::time_point<Clock, Dur>& tp, Tup& locks, std::index_sequence<I0, Is...>,
-                                   const End end) {
-        // try locking the first until tp
-        if(not std::get<I0>(locks).try_lock_until(tp)) return rec_res::timeout;
+        template<size_t I0, size_t... Is, class End, class... Seqs>
+        struct rot_help<std::index_sequence<I0, Is...>, End, Seqs...> :
+            rot_help<std::index_sequence<Is..., I0>, End, Seqs..., std::index_sequence<I0, Is...>> {};
 
-        // try locking the rest using a plain try_lock:
-        try {
-            if(std::try_lock(std::get<Is>(locks)...)) return rec_res::yes;
-        } catch(...) { // locking threw, unlock and rethrow
-            std::get<I0>(locks).unlock();
-            throw;
-        }
-        // locking failed gracefully
-        std::get<I0>(locks).unlock();
+        template<size_t I0, size_t... Is, class... Seqs>
+        struct rot_help<std::index_sequence<I0, Is...>, std::index_sequence<I0, Is...>, Seqs...> {
+            using type = std::tuple<Seqs...>;
+        };
+    } // namespace rot_detail
 
-        // no lock held, do we have a timeout?
-        if(Clock::now() >= tp) return rec_res::timeout;
+    template<size_t N>
+    using make_tuple_of_rotating_index_sequences = rot_detail::rot_help<std::make_index_sequence<N>>::type;
+    //-------------------------------------------------------------------------
+    enum class rec_res { fail, success, timeout };
 
-        // rotate and recurse or have we already tried all rotations?
-        constexpr std::index_sequence<Is..., I0> next;
-        if constexpr(std::same_as<decltype(next), decltype(end)>) {
-            // all rotations exhausted, back out to try again
-            return rec_res::no;
-        } else {
-            return try_lock_until_rec_rot(tp, locks, next, end);
-        }
-    }
+    template<class Timepoint, class Locks, class... Seqs>
+    bool try_lock_until_impl(const Timepoint& tp, Locks locks, std::tuple<Seqs...>) {
+        auto unlock = [&]<std::size_t... Is>(std::index_sequence<Is...>, std::size_t lock_co) {
+            (... && ((std::get<Is>(locks).unlock(), true) && --lock_co));
+        };
 
-    template<class Clock, class Dur, std::size_t... Is, class... Ls>
-    bool try_lock_until_impl(const std::chrono::time_point<Clock, Dur>& tp, std::index_sequence<Is...> is, Ls&... ls) {
-        std::tuple<Ls&...> locks{ls...};
-        while(true) {
-            const auto res = try_lock_until_rec_rot(tp, locks, is, is);
-            if(res == rec_res::yes) return true;
-            if(res == rec_res::timeout) return false;
-        }
+        auto try_until = [&]<std::size_t I0, size_t... Is>(std::index_sequence<I0, Is...> is) {
+            if(not std::get<I0>(locks).try_lock_until(tp)) return rec_res::timeout;
+
+            std::size_t lock_co = 1; // the first is locked
+            try {                    // locking the rest using a plain try_lock:
+                if((... && (std::get<Is>(locks).try_lock() && ++lock_co))) return rec_res::success;
+            } catch(...) { // locking threw, unlock and rethrow
+                unlock(is, lock_co);
+                throw;
+            }
+            // locking failed gracefully
+            unlock(is, lock_co);
+            return rec_res::fail;
+        };
+
+        // try all the rotations in Seqs until one doesn't return rec_res::fail
+        rec_res res;
+        while(not(... || ((res = try_until(Seqs{})) != rec_res::fail)));
+        if(res == rec_res::success) return true;
+        return false; // rec_res::timeout
     }
 } // namespace detail
 
 template<class Clock, class Duration, class L1, class L2, class... Ls>
     requires(detail::TimedLockable<L1> && detail::TimedLockable<L2> && (... && detail::TimedLockable<Ls>))
 [[nodiscard]] bool try_lock_until(const std::chrono::time_point<Clock, Duration>& tp, L1& l1, L2& l2, Ls&... ls) {
-    return detail::try_lock_until_impl(tp, std::make_index_sequence<sizeof...(Ls) + 2>{}, l1, l2, ls...);
+    return detail::try_lock_until_impl(tp, std::tie(l1, l2, ls...),
+                                       detail::make_tuple_of_rotating_index_sequences<sizeof...(Ls) + 2>{});
 }
 
 template<class Rep, class Period, class L1, class L2, class... Ls>
