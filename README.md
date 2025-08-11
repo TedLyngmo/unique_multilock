9e79091d768076e040117de332d0fa225ad40102
Ted Lyngmo <ted@lyncon.se>
2025-08-11 14:29:59 +0200

    Replace try/catch/throw with a tuple of unique_locks
    The unique_locks are placed in the tuple in reverse order and are then
    locked from last to first. This assures unlocking happens in the reverse
    order in case try_lock throws or fails.
    
    Signed-off-by: Ted Lyngmo <ted@lyncon.se>

--- README.md
+++ README.md
@@ -6,10 +6,12 @@ A class which is a combination of `std::unique_lock` and `std::scoped_lock` for 
 This repo also contains a header, `try_lock_for_until.hpp`, with two freestanding functions where two or more timed mutexes can be locked using an algorithm similar to that which is used in `std::lock`:
 ```
 template<class Clock, class Duration, class L1, class L2, class... Ls>
-[[nodiscard]] bool try_lock_until(const std::chrono::time_point<Clock, Duration>& tp, L1& l1, L2& l2, Ls&... ls);
+[[nodiscard]] bool try_lock_until(const std::chrono::time_point<Clock, Duration>& tp,
+                                  L1& l1, L2& l2, Ls&... ls);
 ```
 
 ```
 template<class Rep, class Period, class L1, class L2, class... Ls>
-[[nodiscard]] bool try_lock_for(const std::chrono::duration<Rep, Period>& dur, L1& l1, L2& l2, Ls&... ls);
+[[nodiscard]] bool try_lock_for(const std::chrono::duration<Rep, Period>& dur,
+                                L1& l1, L2& l2, Ls&... ls);
 ```
--- include/try_lock_for_until.hpp
+++ include/try_lock_for_until.hpp
@@ -2,10 +2,13 @@
 #define TRY_LOCK_FOR_UNTIL_HPP_6FE470F4_744E_11F0_BE3A_90B11C0C0FF8
 #include "detail/concepts.hpp"
 
+#include <array>
 #include <chrono>
 #include <cstdint>
+#include <mutex>
 #include <tuple>
 #include <utility>
+
 namespace lyn {
 namespace detail {
     namespace rot_detail {
@@ -34,47 +37,64 @@ namespace detail {
     template<size_t N>
     using make_tuple_of_rotating_index_sequences = rot_detail::rot_help<std::make_index_sequence<N>>::type;
     //-------------------------------------------------------------------------
-    enum class rec_res { fail, success, timeout };
-
+    namespace rev_detail {
+        template<template<class A, A...> class C, class T, T... I, std::size_t... J>
+        constexpr auto rev_help(C<T, I...> const&, std::index_sequence<J...>)
+        // -> C<T, std::get<sizeof...(J) - J - 1>(std::tuple{I...})...>;
+        { // workaround for MSVC:
+            constexpr std::array<T, sizeof...(I)> arr{I...};
+            return C<T, arr[sizeof...(J) - J - 1]...>{};
+        }
+    } // namespace rev_detail
+    template<class T>
+    using reverse_sequence = decltype(rev_detail::rev_help(std::declval<T>(), std::make_index_sequence<T::size()>{}));
+    //-------------------------------------------------------------------------
+    enum class rec_res { fail = -2, success = -1 };
+    template<std::size_t I>
+    constexpr rec_res timeout = static_cast<rec_res>(static_cast<int>(I));
+    //-------------------------------------------------------------------------
     template<class Timepoint, class Locks, class... Seqs>
-    bool try_lock_until_impl(const Timepoint& tp, Locks locks, std::tuple<Seqs...>) {
-        auto unlock = [&]<std::size_t... Is>(std::index_sequence<Is...>, std::size_t lock_co) {
-            (... && ((std::get<Is>(locks).unlock(), true) && --lock_co));
-        };
-
-        auto try_until = [&]<std::size_t I0, size_t... Is>(std::index_sequence<I0, Is...> is) {
-            if(not std::get<I0>(locks).try_lock_until(tp)) return rec_res::timeout;
-
-            std::size_t lock_co = 1; // the first is locked
-            try {                    // locking the rest using a plain try_lock:
-                if((... && (std::get<Is>(locks).try_lock() && ++lock_co))) return rec_res::success;
-            } catch(...) { // locking threw, unlock and rethrow
-                unlock(is, lock_co);
-                throw;
+    int try_lock_until_impl(const Timepoint& tp, Locks locks, std::tuple<Seqs...>) {
+        auto try_once = []<class TUL, size_t... RIs>(TUL ul, std::index_sequence<RIs...>) {
+            // lock from last to first in the tuple of unique_locks
+            if((... && std::get<RIs>(ul).try_lock())) {
+                (..., std::get<RIs>(ul).release());
+                return true;
             }
-            // locking failed gracefully
-            unlock(is, lock_co);
-            return rec_res::fail;
+            return false; // all unlocked in the reverse locking order
+        };
+        auto try_until = [&]<std::size_t I0, size_t... Is>(std::index_sequence<I0, Is...>) {
+            if(std::unique_lock first{std::get<I0>(locks), tp}) {
+                return [&]<std::size_t... RIs>(std::index_sequence<RIs...>) {
+                    // create a tuple of unique_locks in reverse order
+                    if(try_once(std::tuple{std::unique_lock{std::get<RIs>(locks), std::defer_lock}...},
+                                reverse_sequence<std::make_index_sequence<sizeof...(RIs)>>{})) {
+                        first.release();
+                        return rec_res::success;
+                    }
+                    return rec_res::fail;
+                }(reverse_sequence<std::index_sequence<Is...>>{});
+            }
+            return timeout<I0>;
         };
 
         // try all the rotations in Seqs until one doesn't return rec_res::fail
         rec_res res;
         while(not(... || ((res = try_until(Seqs{})) != rec_res::fail)));
-        if(res == rec_res::success) return true;
-        return false; // rec_res::timeout
+        return static_cast<int>(res);
     }
 } // namespace detail
 
 template<class Clock, class Duration, class L1, class L2, class... Ls>
     requires(detail::TimedLockable<L1> && detail::TimedLockable<L2> && (... && detail::TimedLockable<Ls>))
-[[nodiscard]] bool try_lock_until(const std::chrono::time_point<Clock, Duration>& tp, L1& l1, L2& l2, Ls&... ls) {
+[[nodiscard]] int try_lock_until(const std::chrono::time_point<Clock, Duration>& tp, L1& l1, L2& l2, Ls&... ls) {
     return detail::try_lock_until_impl(tp, std::tie(l1, l2, ls...),
                                        detail::make_tuple_of_rotating_index_sequences<sizeof...(Ls) + 2>{});
 }
 
 template<class Rep, class Period, class L1, class L2, class... Ls>
     requires(detail::TimedLockable<L1> && detail::TimedLockable<L2> && (... && detail::TimedLockable<Ls>))
-[[nodiscard]] bool try_lock_for(const std::chrono::duration<Rep, Period>& dur, L1& l1, L2& l2, Ls&... ls) {
+[[nodiscard]] int try_lock_for(const std::chrono::duration<Rep, Period>& dur, L1& l1, L2& l2, Ls&... ls) {
     return try_lock_until(std::chrono::steady_clock::now() + dur, l1, l2, ls...);
 }
 } // namespace lyn
