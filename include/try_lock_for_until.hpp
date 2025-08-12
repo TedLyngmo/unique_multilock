52a86d620cc6c3b44580f809e54728a8678c169c
Ted Lyngmo <ted@lyncon.se>
2025-08-12 17:04:18 +0200

    Code review fixes
    Some things cherry-picked from
    https://codereview.stackexchange.com/a/297885/187147
    
    * It can now lock 0 or more lockables.
    * Moved constraints from requires clause to the template parameter list.
    * Using `int` constants for `fail` and `success`. I kept the `timeout`
      cast helper from the `size_t` lockable index to `int`.
    
    Signed-off-by: Ted Lyngmo <ted@lyncon.se>

--- include/try_lock_for_until.hpp
+++ include/try_lock_for_until.hpp
@@ -7,7 +7,6 @@
 #include <mutex>
 #include <tuple>
 #include <utility>
-
 namespace lyn {
 namespace detail {
     namespace rot_detail {
@@ -19,6 +18,16 @@ namespace detail {
             using type = std::tuple<>;
         };
 
+        template<std::size_t I0> // extra terminator for MSVC
+        struct rot_help<std::index_sequence<I0>> {
+            using type = std::tuple<std::index_sequence<I0>>;
+        };
+
+        template<size_t I0, size_t... Is, class... Seqs>
+        struct rot_help<std::index_sequence<I0, Is...>, std::index_sequence<I0, Is...>, Seqs...> {
+            using type = std::tuple<Seqs...>;
+        };
+
         template<size_t I0, size_t... Is>
         struct rot_help<std::index_sequence<I0, Is...>> :
             rot_help<std::index_sequence<Is..., I0>, std::index_sequence<I0, Is...>, std::index_sequence<I0, Is...>> {};
@@ -26,51 +35,62 @@ namespace detail {
         template<size_t I0, size_t... Is, class End, class... Seqs>
         struct rot_help<std::index_sequence<I0, Is...>, End, Seqs...> :
             rot_help<std::index_sequence<Is..., I0>, End, Seqs..., std::index_sequence<I0, Is...>> {};
-
-        template<size_t I0, size_t... Is, class... Seqs>
-        struct rot_help<std::index_sequence<I0, Is...>, std::index_sequence<I0, Is...>, Seqs...> {
-            using type = std::tuple<Seqs...>;
-        };
     } // namespace rot_detail
 
     template<size_t N>
     using make_tuple_of_rotating_index_sequences = rot_detail::rot_help<std::make_index_sequence<N>>::type;
     //-------------------------------------------------------------------------
-    enum class rec_res { fail = -2, success = -1 };
-    template<std::size_t I>
-    constexpr rec_res timeout = static_cast<rec_res>(static_cast<int>(I));
+    namespace result {
+        inline constexpr int fail = -2;
+        inline constexpr int success = -1;
+        template<std::size_t I>
+        constexpr int timeout = static_cast<int>(I);
+    } // namespace result
     //-------------------------------------------------------------------------
     template<class Timepoint, class Locks, class... Seqs>
     int try_lock_until_impl(const Timepoint& tp, Locks locks, std::tuple<Seqs...>) {
-        auto try_seq = [&]<std::size_t I0, size_t... Is>(std::index_sequence<I0, Is...>) {
-            if(std::unique_lock first{std::get<I0>(locks), tp}) {
-                if(std::try_lock(std::get<Is>(locks)...) == -1) {
-                    first.release();
-                    return rec_res::success;
-                }
-                return rec_res::fail;
+        if constexpr(sizeof...(Seqs) == 0) {
+            return result::success;
+        } else if constexpr(sizeof...(Seqs) == 1) {
+            if(std::get<0>(locks).try_lock()) {
+                return result::success;
             }
-            return timeout<I0>;
-        };
+            return result::timeout<0>;
+        } else {
+            auto try_seq = [&]<std::size_t I0, size_t... Is>(std::index_sequence<I0, Is...>) {
+                if(std::unique_lock first{std::get<I0>(locks), tp}) {
+                    auto lock_the_rest = [&] {
+                        if constexpr(sizeof...(Is) >= 2) {
+                            return std::try_lock(std::get<Is>(locks)...) == -1;
+                        } else {
+                            return std::get<0>(locks).try_lock();
+                        }
+                    };
+                    if(lock_the_rest()) {
+                        first.release();
+                        return result::success;
+                    }
+                    return result::fail;
+                }
+                return result::timeout<I0>;
+            };
 
-        // try all the rotations in Seqs until one doesn't return rec_res::fail
-        rec_res res;
-        while(not(... || ((res = try_seq(Seqs{})) != rec_res::fail)));
-        return static_cast<int>(res);
+            // try all the rotations in Seqs until one doesn't return result::fail
+            int res;
+            while(not(... || ((res = try_seq(Seqs{})) != result::fail)));
+            return res;
+        }
     }
 } // namespace detail
 
-template<class Clock, class Duration, class L1, class L2, class... Ls>
-    requires(detail::TimedLockable<L1> && detail::TimedLockable<L2> && (... && detail::TimedLockable<Ls>))
-[[nodiscard]] int try_lock_until(const std::chrono::time_point<Clock, Duration>& tp, L1& l1, L2& l2, Ls&... ls) {
-    return detail::try_lock_until_impl(tp, std::tie(l1, l2, ls...),
-                                       detail::make_tuple_of_rotating_index_sequences<sizeof...(Ls) + 2>{});
+template<class Clock, class Duration, detail::TimedLockable... Ls>
+[[nodiscard]] int try_lock_until(const std::chrono::time_point<Clock, Duration>& tp, Ls&... ls) {
+    return detail::try_lock_until_impl(tp, std::tie(ls...), detail::make_tuple_of_rotating_index_sequences<sizeof...(Ls)>{});
 }
 
-template<class Rep, class Period, class L1, class L2, class... Ls>
-    requires(detail::TimedLockable<L1> && detail::TimedLockable<L2> && (... && detail::TimedLockable<Ls>))
-[[nodiscard]] int try_lock_for(const std::chrono::duration<Rep, Period>& dur, L1& l1, L2& l2, Ls&... ls) {
-    return try_lock_until(std::chrono::steady_clock::now() + dur, l1, l2, ls...);
+template<class Rep, class Period, detail::TimedLockable... Ls>
+[[nodiscard]] int try_lock_for(const std::chrono::duration<Rep, Period>& dur, Ls&... ls) {
+    return try_lock_until(std::chrono::steady_clock::now() + dur, ls...);
 }
 } // namespace lyn
 #endif
