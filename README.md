69288a5c9a7d0b8c1b17a96a69085002ed8bb8d6
Ted Lyngmo <ted@lyncon.se>
2025-08-13 22:20:09 +0200

    try_lock_for/util: Return bool instead of index
    Signed-off-by: Ted Lyngmo <ted@lyncon.se>

--- README.md
+++ README.md
@@ -3,15 +3,13 @@ A class which is a combination of `std::unique_lock` and `std::scoped_lock` for 
 
 ## `lyn::try_lock_for` and `lyn::try_lock_until`
 
-This repo also contains a header, `try_lock_for_until.hpp`, with two freestanding functions where two or more timed mutexes can be locked using an algorithm similar to that which is used in `std::lock`:
+This repo also contains a header, `try_lock_for_until.hpp`, with two freestanding functions where zero or more timed mutexes can be locked using an algorithm similar to that which is used in `std::lock`:
 ```
-template<class Clock, class Duration, class L1, class L2, class... Ls>
-[[nodiscard]] bool try_lock_until(const std::chrono::time_point<Clock, Duration>& tp,
-                                  L1& l1, L2& l2, Ls&... ls);
+template<class Clock, class Duration, detail::TimedLockable... Ls>
+[[nodiscard]] bool try_lock_until(const std::chrono::time_point<Clock, Duration>& tp, Ls&... ls);
 ```
 
 ```
-template<class Rep, class Period, class L1, class L2, class... Ls>
-[[nodiscard]] bool try_lock_for(const std::chrono::duration<Rep, Period>& dur,
-                                L1& l1, L2& l2, Ls&... ls);
+template<class Rep, class Period, detail::TimedLockable... Ls>
+[[nodiscard]] bool try_lock_for(const std::chrono::duration<Rep, Period>& dur, Ls&... ls);
 ```
--- include/try_lock_for_until.hpp
+++ include/try_lock_for_until.hpp
@@ -1,6 +1,7 @@
 #ifndef TRY_LOCK_FOR_UNTIL_HPP_6FE470F4_744E_11F0_BE3A_90B11C0C0FF8
 #define TRY_LOCK_FOR_UNTIL_HPP_6FE470F4_744E_11F0_BE3A_90B11C0C0FF8
 #include "detail/concepts.hpp"
+
 #include <array>
 #include <chrono>
 #include <cstdint>
@@ -42,15 +43,13 @@ namespace detail {
     using make_tuple_of_rotating_index_sequences = rot_detail::rot_help<std::make_index_sequence<N>>::type;
     //-------------------------------------------------------------------------
     namespace result {
-        inline constexpr int fail = -2;
-        inline constexpr int success = -1;
-        template<std::size_t I>
-        constexpr int timeout = static_cast<int>(I);
+        inline constexpr auto timeout = static_cast<std::size_t>(-2);
+        inline constexpr auto success = static_cast<std::size_t>(-1);
+        inline bool is_index(size_t x) {
+            return x < timeout;
+        }
     } // namespace result
     //-------------------------------------------------------------------------
-    constexpr int friendly_try_lock() {
-        return -1;
-    }
     int friendly_try_lock(auto& l1) {
         return -static_cast<int>(l1.try_lock());
     }
@@ -59,41 +58,42 @@ namespace detail {
     }
     //-------------------------------------------------------------------------
     template<class Clock, class Duration, class Locks, std::size_t I0, size_t... Is>
-    int try_sequence(std::size_t& next, const std::chrono::time_point<Clock, Duration>& tp, Locks& locks,
-                     std::index_sequence<I0, Is...>) {
+    std::size_t try_sequence(const std::chrono::time_point<Clock, Duration>& tp, Locks& locks, std::index_sequence<I0, Is...>) {
         do {
             if(std::unique_lock first{std::get<I0>(locks), tp}) {
-                int res = friendly_try_lock(std::get<Is>(locks)...);
-                if(res == result::success) {
+                if constexpr(sizeof...(Is) == 0) {
                     first.release();
                     return result::success;
-                }
-                if constexpr(sizeof...(Is) != 0) {
+                } else {
+                    int res = friendly_try_lock(std::get<Is>(locks)...);
+                    if(res == -1) {
+                        first.release();
+                        return result::success;
+                    }
                     // try to unique_lock the failing one next
-                    next = std::array{Is...}[static_cast<std::size_t>(res)];
+                    return std::array{Is...}[static_cast<std::size_t>(res)];
                 }
-                return result::fail;
             }
         } while(Clock::now() < tp); // retry if spurious return
-        return result::timeout<I0>;
+        return result::timeout;
     }
     //-------------------------------------------------------------------------
-    template<class Clock, class Duration, class Locks, class... Seqs>
-    int try_lock_until_impl(const std::chrono::time_point<Clock, Duration>& end_time, Locks locks, std::tuple<Seqs...>) {
-        using func_sig = int (*)(std::size_t&, const std::chrono::time_point<Clock, Duration>&, Locks&);
+    template<class Timepoint, class Locks, class... Seqs>
+    bool try_lock_until_impl(const Timepoint& end_time, Locks locks, std::tuple<Seqs...>) {
+        using func_sig = std::size_t (*)(const Timepoint&, Locks&);
 
-        std::array<func_sig, sizeof...(Seqs)> seqs{{{+[](std::size_t& next, const std::chrono::time_point<Clock, Duration>& tp,
-                                                         Locks& lks) { return try_sequence(next, tp, lks, Seqs{}); }}...}};
+        std::array<func_sig, sizeof...(Seqs)> seqs{
+            {{+[](const Timepoint& tp, Locks& lks) { return try_sequence(tp, lks, Seqs{}); }}...}};
 
-        // try rotations in seqs while they return result::fail
-        int res = 0;
-        for(size_t next = 0; (res = seqs[next](next, end_time, locks)) == result::fail;);
-        return res;
+        // try rotations in seqs while they return an index sequence to try next
+        size_t ret = 0;
+        while(result::is_index(ret = seqs[ret](end_time, locks)));
+        return ret == result::success;
     }
 } // namespace detail
 
 template<class Clock, class Duration, detail::TimedLockable... Ls>
-[[nodiscard]] int try_lock_until(const std::chrono::time_point<Clock, Duration>& tp, Ls&... ls) {
+[[nodiscard]] bool try_lock_until(const std::chrono::time_point<Clock, Duration>& tp, Ls&... ls) {
     if constexpr(sizeof...(Ls) == 0) {
         return detail::result::success;
     } else {
@@ -102,7 +102,7 @@ template<class Clock, class Duration, detail::TimedLockable... Ls>
 }
 
 template<class Rep, class Period, detail::TimedLockable... Ls>
-[[nodiscard]] int try_lock_for(const std::chrono::duration<Rep, Period>& dur, Ls&... ls) {
+[[nodiscard]] bool try_lock_for(const std::chrono::duration<Rep, Period>& dur, Ls&... ls) {
     return try_lock_until(std::chrono::steady_clock::now() + dur, ls...);
 }
 } // namespace lyn
