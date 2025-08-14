201a337a53154ba0d06be172b1a8c1b918ed94fe
Ted Lyngmo <ted@lyncon.se>
2025-08-14 17:08:04 +0200

    Improve make_pack_of_rotating_index_sequences
    This uses Artyer's implementation:
    https://stackoverflow.com/a/79735399/7582247
    
    MSVC now maxes out at N=394. clang, icx and now also gcc manages 700+
    rotations.
    
    Signed-off-by: Ted Lyngmo <ted@lyncon.se>

--- include/try_lock_for_until.hpp
+++ include/try_lock_for_until.hpp
@@ -14,40 +14,30 @@ namespace detail {
     template<class...>
     struct type_pack {}; // seems to compile slightly faster than using a tuple
     namespace rot_detail {
-        template<class...>
-        struct rot_help;
+        template<std::size_t I, class IndexSeq>
+        struct rotate;
 
-        template<>
-        struct rot_help<std::index_sequence<>> {
-            using type = type_pack<>;
+        template<std::size_t I, std::size_t... J>
+        struct rotate<I, std::index_sequence<J...>> {
+            using type = std::index_sequence<((J + I) % sizeof...(J))...>;
         };
 
-        template<size_t I0, size_t... Is, class... Seqs> // term
-        struct rot_help<std::index_sequence<I0, Is...>, std::index_sequence<I0, Is...>, Seqs...> {
-            using type = type_pack<Seqs...>;
+        template<class IndexSeq>
+        struct make_pack_impl;
+
+        template<std::size_t... I>
+        struct make_pack_impl<std::index_sequence<I...>> {
+            using type = type_pack<typename rotate<I, std::index_sequence<I...>>::type...>;
         };
-
-        template<std::size_t I0> // extra terminator for MSVC
-        struct rot_help<std::index_sequence<I0>> {
-            using type = type_pack<std::index_sequence<I0>>;
-        };
-
-        template<size_t I0, size_t... Is>
-        struct rot_help<std::index_sequence<I0, Is...>> :
-            rot_help<std::index_sequence<Is..., I0>, std::index_sequence<I0, Is...>, std::index_sequence<I0, Is...>> {};
-
-        template<size_t I0, size_t... Is, class End, class... Seqs>
-        struct rot_help<std::index_sequence<I0, Is...>, End, Seqs...> :
-            rot_help<std::index_sequence<Is..., I0>, End, Seqs..., std::index_sequence<I0, Is...>> {};
     } // namespace rot_detail
 
-    template<size_t N>
-    using make_pack_of_rotating_index_sequences = rot_detail::rot_help<std::make_index_sequence<N>>::type;
+    template<std::size_t N>
+    using make_pack_of_rotating_index_sequences = typename rot_detail::make_pack_impl<std::make_index_sequence<N>>::type;
     //-------------------------------------------------------------------------
     namespace result {
         inline constexpr auto timeout = static_cast<std::size_t>(-2);
         inline constexpr auto success = static_cast<std::size_t>(-1);
-        inline bool is_index(size_t x) {
+        inline bool is_index(std::size_t x) {
             return x < timeout;
         }
     } // namespace result
@@ -59,7 +49,7 @@ namespace detail {
         return std::try_lock(ls...);
     }
     //-------------------------------------------------------------------------
-    template<class Clock, class Duration, class Locks, std::size_t I0, size_t... Is>
+    template<class Clock, class Duration, class Locks, std::size_t I0, std::size_t... Is>
     std::size_t try_sequence(const std::chrono::time_point<Clock, Duration>& tp, Locks& locks, std::index_sequence<I0, Is...>) {
         do {
             if constexpr(sizeof...(Is) == 0) {
@@ -87,7 +77,7 @@ namespace detail {
             {{+[](const Timepoint& tp, Locks& lks) { return try_sequence(tp, lks, Seqs{}); }}...}};
 
         // try rotations in seqs while they return an index sequence to try next
-        size_t ret = 0;
+        std::size_t ret = 0;
         while(result::is_index(ret = seqs[ret](end_time, locks)));
         return ret == result::success;
     }
